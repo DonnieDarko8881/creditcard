65b20f720eaf8086d2ecc804b5ad74f2546f0994
szymon <szymon.medykowski@newspoint.pl>
2020-12-04 16:37:41 +0100

    finish

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { CardFormComponent } from './card-form/card-form.component';
+import { InputComponent } from './input/input.component';
+import { CardComponent } from './card/card.component';
+
+// import { NgxMaskModule, IConfig } from 'ngx-mask'
+
+// export const options: Partial<IConfig> | (() => Partial<IConfig>) = null;
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardFormComponent,
+    InputComponent,
+    CardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    ReactiveFormsModule
+    // NgxMaskModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
